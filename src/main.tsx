5ef5296dbbaa6afc35753e9c0286577ab2b182d4
Rayane <ettaouir1@gmail.com>
2025-05-07 01:07:23 +0100

    Changing ReactBrowser to HashRouter since github doesn't work with it

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import ElectreResultViewer from "./ElectreResultViewer.tsx";
 // import ElectreForm from "./ElectreForm.tsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
