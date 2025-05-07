2594ae7a893aec3f1f8f895f9c76bc52d204c7fa
Rayane <ettaouir1@gmail.com>
2025-05-07 01:02:54 +0100

    Fixing vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/ElectreWizard/",
+  base: "/Electre-1-Decision-Support/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
